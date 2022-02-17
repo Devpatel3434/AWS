d8dca0be7205270419431484c0a52f00e9c9e3bb
Devil <devil@Devils-MacBook-Air.local>
2022-02-17 15:24:05 +0530

    small changes

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, UserMixin
 from sqlalchemy import false
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_admin import Admin , AdminIndexView
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_recaptcha import ReCaptcha
 from flask_wtf import RecaptchaField
@@ -19,7 +19,6 @@ application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(application)
 recaptcha = ReCaptcha(application=application)
-
 
 
 '''
@@ -33,19 +32,17 @@ application.config['MAIL_USE_SSL'] = True
 
 
 application.config.update(dict(
-    RECAPTCHA_ENABLED = True,
-    RECAPTCHA_SITE_KEY = "6LcaCiUeAAAAAE8c5Eb3ADVw-7UPybPHppPl7kpv",
-    RECAPTCHA_SECRET_KEY = "6LcaCiUeAAAAAI5X0blM8ghaB4mzElzJa9hHQw5p",
-    ))
- 
+    RECAPTCHA_ENABLED=True,
+    RECAPTCHA_SITE_KEY="6LcaCiUeAAAAAE8c5Eb3ADVw-7UPybPHppPl7kpv",
+    RECAPTCHA_SECRET_KEY="6LcaCiUeAAAAAI5X0blM8ghaB4mzElzJa9hHQw5p",
+))
+
 recaptcha = ReCaptcha()
 recaptcha.init_app(application)
-#recaptcha.init_application(application)
+# recaptcha.init_application(application)
 
 
-
-
-class User(UserMixin ,db.Model):
+class User(UserMixin, db.Model):
     #id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(100))
     lname = db.Column(db.String(100))
@@ -59,18 +56,20 @@ class User(UserMixin ,db.Model):
         self.password = password
 
 
-@application.route('/', methods=['GET','POST'])
+@application.route('/', methods=['GET', 'POST'])
 def index():
     if session.get('logged_in'):
-        #user=session['user']
+        # user=session['user']
         return redirect(url_for('home'))
     else:
         return render_template('index.html', message="Hello!")
 
 
-@application.route('/home/',methods=['GET','POST'])
+@application.route('/home/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
+
+
 '''
 @application.route('/email/',methods=['GET','POST'])
 def email():
@@ -87,19 +86,14 @@ def verify():
 '''
 
 
-
-
-
-
-
-
 @application.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         try:
-            password, cnfpassword=request.form['password'], request.form['cnfpassword']
-            if(password==cnfpassword):
-                db.session.add(User(fname=request.form['fname'], lname=request.form['lname'], email=request.form['email'], password=generate_password_hash(password, method='sha256')))
+            password, cnfpassword = request.form['password'], request.form['cnfpassword']
+            if(password == cnfpassword):
+                db.session.add(User(fname=request.form['fname'], lname=request.form['lname'],
+                               email=request.form['email'], password=generate_password_hash(password, method='sha256')))
                 db.session.commit()
                 return redirect(url_for('login'))
             else:
@@ -109,38 +103,39 @@ def register():
     else:
         return render_template('register.html')
 
+
 application.config.from_pyfile('config.cfg')
 
 mail = Mail(application)
 otp = randint(0000, 9999)
 s = URLSafeTimedSerializer('Thisisasecret!')
 
+
 @application.route('/login', methods=['GET', 'POST'])
 def login():
-    
+
     if request.method == 'GET':
         return render_template('login.html')
     else:
         email = request.form['email']
         password = request.form['password']
-        
 
         data = User.query.filter_by(email=email).first()
 
-
         if (data and check_password_hash(data.password, password)) and recaptcha.verify():
-            msg = Message('Confirm Email', sender='devnpatel18@gnu.ac.in', recipients=[email])
-            msg.body = str(otp) #'Your link is {}'.format(link)
+            msg = Message('Confirm Email',
+                          sender='devnpatel18@gnu.ac.in', recipients=[email])
+            msg.body = str(otp)  # 'Your link is {}'.format(link)
             mail.send(msg)
             return redirect('/confirm')
             #session['logged_in'] = True
-            #return redirect(url_for('index'))
-        elif(data==None):
-            return render_template('login.html',message="Email is not registered")
+            # return redirect(url_for('index'))
+        elif(data == None):
+            return render_template('login.html', message="Email is not registered")
         elif not (data and check_password_hash(data.password, password)):
-            return render_template('login.html',message="Email and password don't match")
-    
-        return render_template('login.html', message="Recheck the Field") 
+            return render_template('login.html', message="Email and password don't match")
+
+        return render_template('login.html', message="Recheck the Field")
 
 
 @application.route('/logout', methods=['GET', 'POST'])
@@ -171,19 +166,22 @@ def indexs():
 
     #return '<h1>The email you entered is {}.</h1>'.format(email) 
 '''
+
+
 @application.route('/confirm', methods=['GET', 'POST'])
 def confirm():
     if request.method == 'GET':
         return render_template('verify.html')
      #   return '<form action="/confirm" method="POST"><input name="otp"><input type="submit"></form>'
 
-    userotp=request.form['otp']
-    if otp== int(userotp):
-        #session['logged_in'] = True
-        #return redirect(url_for('index'))
-        return render_template('home.html')
-        #return " Email verified Success" 
+    userotp = request.form['otp']
+    if otp == int(userotp):
+        session['logged_in'] = True
+        return redirect(url_for('index'))
+        # return render_template('home.html')
+        # return " Email verified Success"
     return redirect('/confirm')
+
 
 admin = Admin(application)
 admin.add_view(ModelView(User, db.session))
